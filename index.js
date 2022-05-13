6a584f352cfd1c65f86cf26310e6f744d3be3b6c
Sam Carlton <sam@sam.lc>
2022-05-13 14:43:15 -0500

    Add element selector list

--- index.js
+++ index.js
@@ -13,8 +13,92 @@
 
         }
 
+        // https://www.w3.org/TR/2017/NOTE-wai-aria-practices-1.1-20171214/examples/landmarks/HTML5.html
+        elementDetails = [
+            // Landmarks
+
+            // complementary
+            [
+                'aside:not([role]), [role~="complementary"], [role~="COMPLEMENTARY"]',
+                'landmark'
+            ], 
+
+            // contentinfo
+            [
+                `${ this.contentInfoOrBannerSelector } footer, [role~="contentinfo"], [role~="CONTENTINFO"]`, 
+                'landmark'
+            ], 
+            
+            // application
+            [
+                '[role~="application"], [role~="APPLICATION"]', 
+                'landmark'
+            ],
+
+            // navigation
+            [
+                'nav, [role~="navigation"], [role~="NAVIGATION"]',
+                'landmark'
+            ],
+
+            // region aria-labelledby
+            [
+                '[role~="region"][aria-labelledby], [role~="REGION"][aria-labelledby]', 
+                'landmark'
+            ], 
+
+            // region aria-label
+            [
+                '[role~="region"][aria-label], [role~="REGION"][aria-label]', 
+                'landmark'
+            ], 
+
+            // region section
+            [
+                'section[aria-labelledby], section[aria-label]', 
+                'landmark'
+            ], 
+
+            // banner
+            [
+                `${ this.contentInfoOrBannerSelector } header, [role~="banner"], [role~="BANNER"]`, 
+                'landmark'
+            ],
+
+            // search
+            [
+                '[role~="search"], [role~="SEARCH"]', 
+                'landmark'
+            ], 
+
+            // main
+            [
+                'main, [role~="main"], [role~="MAIN"]',
+                'landmark'
+            ], 
+
+
+            // Headings
+            [
+                'h1, h2, h3, h4, h5, h6, [role~="heading"], [role~="HEADING"]',
+                'heading'
+            ], 
+
+            // Links
+            [
+                'a, [role~="link"], [role~="LINK"]',
+                'link', 
+            ]
+        ]
+
+
 
         contentInfoOrBannerExceptions = [ 'article', 'aside', 'main', 'nav', 'section' ]
+        contentInfoOrBannerSelector = this.notSelectors( this.contentInfoOrBannerExceptions ).join('')
+
+        notSelectors ( selectors ) {
+            return selectors.map( selector => `:not(${selector})` )
+        }
 
         isDescendantOf (element, tagNames) {
             if (typeof element.closest === 'function') {
