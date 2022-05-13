1feb1e8bf5fad19608213609df64be32fc446524
Sam Carlton <sam@sam.lc>
2022-05-13 13:47:16 -0500

    Add UniversalKeyboardNavigator class

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
+
+
 ;(function (){
-    console.log('Works!')
+    class UniversalKeyboardNavigator {
+        constructor ( options = {} ) {
+            const {
+                selectors = {}
+            } = options
+
+
+        }
+
+        initialize() {
+            // Delete any existing instances
+            console.log('UniversalKeyboardNavigator initialized')
+        }
+    }
+
+    // Create and Initialize the keyboard navigator
+    window.universalKeyboardNavigator = new UniversalKeyboardNavigator()
+    window.universalKeyboardNavigator.initialize()
+
 })()
